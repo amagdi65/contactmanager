af61c84bd546d97d035fac8dd35128304966a410
ahmed magdy <a.magdi65@gamil.com>
2019-06-18 04:35:37 +0200

    Updates

---
+++ precache-manifest.88730d6d027be39818fa339fa265f2dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876758d401898641dc41ebc6445b3920",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "27e8f09d731e17aaf28c",
+    "url": "/contactmanager/static/css/2.81b134f3.chunk.css"
+  },
+  {
+    "revision": "27e8f09d731e17aaf28c",
+    "url": "/contactmanager/static/js/2.ff95717f.chunk.js"
+  },
+  {
+    "revision": "35549576a035be9cc094",
+    "url": "/contactmanager/static/js/main.cf1c5125.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
